9c97039ae0390b59cb8925a183ff4827c7bf5a57
vivek kumar singh <72482390+cyphon-tech@users.noreply.github.com>
2021-08-26 09:17:31 +0530

    completed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ export default function Navbar(props) {
         <nav className={`navbar navbar-expand-lg navbar-${props.Mode} bg-${props.Mode}`}>
         <div className="container-fluid">
           {/* <Link className="navbar-brand" to="/">{props.title}</Link> */}
-          <a className="navbar-brand" href="/">{props.title}</a>
+          <a className="navbar-brand" href="#">{props.title}</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
@@ -16,11 +16,11 @@ export default function Navbar(props) {
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
                 {/* <Link className="nav-link active" aria-current="page" to="/">Home</Link> */}
-                <a className="nav-link active" aria-current="page" href="/">Home</a>
+                <a className="nav-link active" aria-current="page" href="#">Home</a>
               </li>
               <li className="nav-item">
                 {/* <Link className="nav-link active" aria-current="page" to="/about">{props.aboutText}</Link> */}
-                <a className="nav-link active" aria-current="page" href="/about">{props.aboutText}</a>
+                <a className="nav-link active" aria-current="page" href="#">{props.aboutText}</a>
               </li>
      
             </ul>
@@ -40,6 +40,6 @@ Navbar.prototype = {
 }
 
 // Navbar.defaultProps={
-//     title:'set',
+//     title:'set',   
 //     aboutText:'set about'
 // }
